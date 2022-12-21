71cd12a4e342861adbae45064d096b9b2a16371b
Koziad <kostas1999NL@gmail.com>
2022-12-21 13:58:35 +0100

    Add test dependencies + sample test created for the pipeline

---
+++ identity-and-access/tests/integration/test_register.py
@@ -0,0 +1,23 @@
+import os
+from unittest.mock import patch
+
+OS_ENV = {
+    "AWS_ACCESS_KEY_ID": "testing",
+    "AWS_SECRET_ACCESS_KEY": "testing",
+    "AWS_SECURITY_TOKEN": "testing",
+    "AWS_SESSION_TOKEN": "testing",
+    "AWS_DEFAULT_REGION": "us-east-1",
+    "DISABLE_XRAY": "True"
+}
+
+
+@patch.dict(os.environ, OS_ENV, clear=True)
+def test_lambda_handler():
+    # Arrange
+    # Act
+    from register.app import lambda_handler
+
+    response = lambda_handler({}, {})
+
+    # Assert
+    assert response["statusCode"] == 201
