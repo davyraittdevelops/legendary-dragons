3cf1a871bd95af73d5db202528f91b94324ba044
Evripidou <EvripidouNL@gmail.com>
2022-12-22 16:13:17 +0100

    Add login function with behave and integration test

--- functional-tests/features/identity-and-access.feature
+++ functional-tests/features/identity-and-access.feature
@@ -1,6 +1,11 @@
 Feature: Identity & Access Context
 
 Scenario: Registering a new user
-    Given we have a new user
+    Given we have no user
     When we register with email 'kostas.mylothridis@student.hu.nl' and password 'Th3bestPassword!'
     Then the user should be registered
+
+Scenario: Login an existing user
+    Given we have a registered user with email 'kostas.mylothridis@student.hu.nl' and password 'Th3bestPassword!'
+    When we login the existing user
+    Then the user should be logged in
--- functional-tests/features/steps/identity-and-access.py
+++ functional-tests/features/steps/identity-and-access.py
@@ -3,17 +3,19 @@ import requests
 import logging
 import uuid
 import json
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+client = boto3.client("cognito-idp", region_name="us-east-1")
 
-@given("we have a new user")
+@given("we have no user")
 def step_impl(context):
     context.nickname = uuid.uuid4().hex
     logger.info(f"Generated nickname: ${context.nickname}")
 
-
+@given("we have a registered user with email '{email}' and password '{password}'")
 @when("we register with email '{email}' and password '{password}'")
 def step_impl(context, email, password):
     body = {"nickname": context.nickname, "email": email, "password": password}
@@ -25,8 +27,28 @@ def step_impl(context, email, password):
     context.detail["email"] = email
     context.status_code = response.status_code
 
+@when("we login the existing user")
+def step_impl(context):
+    response = client.initiate_auth(
+    ClientId="55h918ad6191srnlgj0duprpf3",
+    AuthFlow='USER_PASSWORD_AUTH',
+    AuthParameters={
+        "USERNAME": context.detail["email"],
+        "PASSWORD": "Th3bestPassword!"
+    }
+)
+    context.token = response['AuthenticationResult']['IdToken']
 
 @then("the user should be registered")
 def step_impl(context):
-    # TODO: add admin_confirm_sign_up
+    client.admin_confirm_sign_up(
+    UserPoolId="us-east-1_H1AyV4HD1",
+    Username=context.detail["email"])
+
+    logger.info(f"statuscode: ${context.status_code}")
     assert context.status_code == 201
+
+@then("the user should be logged in")
+def step_impl(context):
+    logger.info(f"jwttoken: ${context.token}")
+    assert context.token != ""
