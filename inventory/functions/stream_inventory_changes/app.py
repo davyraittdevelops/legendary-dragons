fa9f54ee7c4027e5e47f28ae3c288bc5478a708a
Koziad <kostas1999NL@gmail.com>
2023-01-10 10:57:40 +0100

    Stream inventory changes lambda created

--- inventory/functions/stream_inventory_changes/app.py
+++ inventory/functions/stream_inventory_changes/app.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 import boto3
+from datetime import datetime
 from aws_xray_sdk.core import patch_all
 
 logger = logging.getLogger()
@@ -11,5 +12,30 @@ if "DISABLE_XRAY" not in os.environ:
     patch_all()
 
 
+events_client = boto3.client("events")
+
+
 def lambda_handler(event, context):
-    return {"status": 200}
+    """Convert inventory DynamoDB streams into events for EventBridge."""
+    events = []
+    logger.info(f"Received Event: {event}")
+
+    for record in event["Records"]:
+        logger.info(f"Event: {record['eventName']} | {record['eventID']}")
+        table_arn = record["eventSourceARN"].split("/stream")[0]
+
+        events.append(
+            {
+                "Time": datetime.utcfromtimestamp(
+                    record["dynamodb"]["ApproximateCreationDateTime"]
+                ),
+                "Source": "inventory.database",
+                "Resources": [table_arn],
+                "DetailType": record["eventName"],
+                "Detail": json.dumps(record),
+                "EventBusName": os.getenv("EVENT_BUS_NAME")
+            }
+        )
+
+    events_client.put_events(Entries=events)
+    return {"statusCode": 200}
