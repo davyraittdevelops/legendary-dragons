50eb8c48ed6c9c198b3e5e805e790a343719f9c2
Koziad <kostas1999NL@gmail.com>
2023-01-10 14:40:10 +0100

    Implement get inventory lambda

--- inventory/functions/get_inventory/app.py
+++ inventory/functions/get_inventory/app.py
@@ -2,7 +2,7 @@ import json
 import logging
 import os
 import boto3
-from datetime import datetime
+from boto3.dynamodb.conditions import Key
 from aws_xray_sdk.core import patch_all
 
 logger = logging.getLogger()
@@ -11,9 +11,41 @@ logger.setLevel(logging.INFO)
 if "DISABLE_XRAY" not in os.environ:
     patch_all()
 
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.getenv("TABLE_NAME"))
+
 
 def lambda_handler(event, context):
-    """Create a new Inventory entry after account confirmation."""
+    """Get a inventory entry for a specific user."""
     logger.info(event)
+    body = json.loads(event["body"])
 
-    return {"statusCode": 200}
+    # TODO: Check how to retrieve user id
+    inventory_id = body["inventory_id"]
+    user_id = body["user_id"]
+
+    inventory = table.get_item(
+        Key={
+            "PK": f"INVENTORY#{inventory_id}",
+            "SK": f"USER#{user_id}"
+        }
+    )
+
+    if "Item" not in inventory:
+        logger.info(f"Inventory with id = {inventory_id} not found")
+        return {"statusCode": 400}
+
+    inventory_cards = table.query(
+        KeyConditionExpression=Key("GSI1_PK").eq(f"INVENTORY#{inventory_id}") &
+        Key("GSI1_SK").begins_with("INVENTORY_CARD"),
+        IndexName="GSI1"
+    )["Items"]
+
+    logger.info(f"Found {len(inventory_cards)} inventory cards")
+    inventory["Item"]["inventory_cards"] = inventory_cards
+
+    return {
+        "statusCode": 200,
+        "event_type": "GET_INVENTORY_RESULT",
+        "data": inventory["Item"]
+    }
