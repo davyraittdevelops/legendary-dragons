7a9c4cdeef8ca9509f84d516e85a13a42f3d0794
Evripidou <EvripidouNL@gmail.com>
2023-01-11 10:13:24 +0100

    Refactor test disconnect and onconnect

--- connection/tests/integration/test_disconnect.py
+++ connection/tests/integration/test_disconnect.py
@@ -6,16 +6,7 @@ import botocore
 import pytest
 from moto import mock_dynamodb
 
-@pytest.fixture()
-def event():
-    return {'requestContext': { 
-        "connectionId": "eiC3NdK8IAMCIYA=",
-        "requestId": "eiC3OH_tIAMF9Mw=",
-        "apiId": "3ghgk1q3mf" ,
-        "domainName": "3ghgk1q3mf.execute-api.us-east-1.amazonaws.com",
-        "stage": "Prod",
-    }}
-
+TABLE_NAME = "connections"
 OS_ENV = {
     "AWS_ACCESS_KEY_ID": "testing",
     "AWS_SECRET_ACCESS_KEY": "testing",
@@ -25,61 +16,70 @@ OS_ENV = {
     "DISABLE_XRAY": "True"
 }
 
+@pytest.fixture()
+def table_definition():
+    return {
+        "TableName": TABLE_NAME,
+        "AttributeDefinitions": [
+            {"AttributeName": "PK", "AttributeType": "S"},
+            {"AttributeName": "SK", "AttributeType": "S"},
+        ],
+        "KeySchema": [
+            {"AttributeName": "PK", "KeyType": "HASH"},
+            {"AttributeName": "SK", "KeyType": "RANGE"}
+        ],
+
+        "BillingMode": "PAY_PER_REQUEST"
+    }
+
+@pytest.fixture()
+def websocket_event():
+    """Generates Websocket Event"""
+    return {
+        "requestContext": {
+            "connectionId": "eiC3NdK8IAMCIYA=",
+            "requestId": "eiC3OH_tIAMF9Mw=",
+            "apiId": "3ghgk1q3mf" ,
+            "domainName": "3ghgk1q3mf.execute-api.us-east-1.amazonaws.com",
+            "stage": "Prod",
+        },
+        "body": json.dumps({
+            "action": "disconnect"
+        }),
+    }
+
+orig = botocore.client.BaseClient._make_api_call
+
+def mock_make_api_call(self, operation_name, kwarg):
+    if operation_name == "PostToConnection":
+        return None
+    return orig(self, operation_name, kwarg)
+
+
 @patch.dict(os.environ, OS_ENV, clear=True)
 @mock_dynamodb
-def test_lambda_handler(event):
-    from functions.disconnect.app import lambda_handler
+def test_lambda_handler(websocket_event, table_definition):
     # Arrange
-    orig = botocore.client.BaseClient._make_api_call
-
     dynamodb = boto3.resource('dynamodb')
-    connectionsTable = dynamodb.create_table(
-        TableName='connections',
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'PK',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'SK',
-                'AttributeType': 'S'
-            }
-        ],
-        KeySchema=[
-            {
-                'AttributeName': 'PK',
-                'KeyType': 'HASH'
-            },
-            {
-                'AttributeName': 'SK',
-                'KeyType': 'RANGE'
-            }
-        ],
-        BillingMode='PAY_PER_REQUEST'
-    )
+    table = dynamodb.create_table(**table_definition)
 
     connectionItemToInsert = {
         "PK": "Connection#eiC3NdK8IAMCIYA=",
         "SK": "User#123",
-        "entity_type": "Connection",
+        "entity_type": "CONNECTION",
         "domain": "3ghgk1q3mf.execute-api.us-east-1.amazonaws.com",
         "stage": "Prod",
         "connection_id": "eiC3NdK8IAMCIYA=",
         "user_id": "123"
     }
 
-    connectionsTable.put_item(Item=connectionItemToInsert)
+    table.put_item(Item=connectionItemToInsert)
 
-    def mock_make_api_call(self, operation_name, kwarg):
-        if operation_name == 'PostToConnection':
-            return None
-        return orig(self, operation_name, kwarg)
-
-    with patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call) as mock_method:
+    with patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call):
         # Act
-        response = lambda_handler(event, {})
-        body = json.dumps(response['body'])
+        from functions.disconnect import app
+        response = app.lambda_handler(websocket_event, {})
+        
         # # Assert
         assert response['statusCode'] == 200
-        assert event["requestContext"]["connectionId"] == "eiC3NdK8IAMCIYA="
-        assert body == '"Disconnected."'
+        assert response['body'] == 'Disconnected.'
