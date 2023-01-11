621d4b4d3e1ead3e6efc044cda6c5ef6e082b369
Salt <evatang2002@hotmail.com>
2023-01-11 14:37:07 +0100

    every deck is now clickable

--- legendary-dragons-frontend/src/app/components/decks-page/decks-page.component.ts
+++ legendary-dragons-frontend/src/app/components/decks-page/decks-page.component.ts
@@ -85,7 +85,62 @@ export class DecksPageComponent implements OnInit {
       total_value: '60000',
       created_at: new Date("2019-01-16"),
       last_modified: new Date("2019-01-16"),
-      cards: []
+      cards: [
+        {
+          name: 'Goblin Guide',
+          type: 'Creature',
+          text: 'Haste. Whenever Goblin Guide attacks, defending player reveals the top card of their library',
+          cost: 1,
+          power: 2,
+          toughness: 2,
+          imageUrl: 'https://www.bazaargames.nl/images/cards/l/zen/goblin_guide.jpg'
+        },
+        {
+          name: 'Lightning Bolt',
+          type: 'Instant',
+          text: 'Lightning Bolt deals 3 damage to any target.',
+          cost: 1,
+          power: 0,
+          toughness: 0,
+          imageUrl: 'https://cards.scryfall.io/large/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721'
+        },
+        {
+          name: 'Goblin Guide',
+          type: 'Creature',
+          text: 'Haste. Whenever Goblin Guide attacks, defending player reveals the top card of their library',
+          cost: 1,
+          power: 2,
+          toughness: 2,
+          imageUrl: 'https://www.bazaargames.nl/images/cards/l/zen/goblin_guide.jpg'
+        },
+        {
+          name: 'Lightning Bolt',
+          type: 'Instant',
+          text: 'Lightning Bolt deals 3 damage to any target.',
+          cost: 1,
+          power: 0,
+          toughness: 0,
+          imageUrl: 'https://cards.scryfall.io/large/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721'
+        },
+        {
+          name: 'Snapcaster Mage',
+          type: 'Creature',
+          text: 'Flash. When Snapcaster Mage enters the battlefield, target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost.',
+          cost: 2,
+          power: 2,
+          toughness: 1,
+          imageUrl: 'https://cards.scryfall.io/large/front/2/b/2b73d294-6ab1-4051-9b0f-d8e335d37674.jpg?1626097096'
+        },
+        {
+          name: 'Remand',
+          type: 'Instant',
+          text: 'Counter target spell. If that spell is countered this way, put it into its owners hand instead of into that players graveyard.',
+          cost: 1,
+          power: 0,
+          toughness: 0,
+          imageUrl: 'https://miro.medium.com/max/672/0*WtTRy5c4r3h_JFVB.jpg'
+        }
+      ]
     },
     {
       name: 'Grixis Midrange',
@@ -149,7 +204,62 @@ export class DecksPageComponent implements OnInit {
       total_value: '10',
       created_at: new Date("2019-01-16"),
       last_modified: new Date("2019-01-16"),
-      cards: []
+      cards: [
+        {
+          name: 'Goblin Guide',
+          type: 'Creature',
+          text: 'Haste. Whenever Goblin Guide attacks, defending player reveals the top card of their library',
+          cost: 1,
+          power: 2,
+          toughness: 2,
+          imageUrl: 'https://www.bazaargames.nl/images/cards/l/zen/goblin_guide.jpg'
+        },
+        {
+          name: 'Lightning Bolt',
+          type: 'Instant',
+          text: 'Lightning Bolt deals 3 damage to any target.',
+          cost: 1,
+          power: 0,
+          toughness: 0,
+          imageUrl: 'https://cards.scryfall.io/large/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721'
+        },
+        {
+          name: 'Goblin Guide',
+          type: 'Creature',
+          text: 'Haste. Whenever Goblin Guide attacks, defending player reveals the top card of their library',
+          cost: 1,
+          power: 2,
+          toughness: 2,
+          imageUrl: 'https://www.bazaargames.nl/images/cards/l/zen/goblin_guide.jpg'
+        },
+        {
+          name: 'Lightning Bolt',
+          type: 'Instant',
+          text: 'Lightning Bolt deals 3 damage to any target.',
+          cost: 1,
+          power: 0,
+          toughness: 0,
+          imageUrl: 'https://cards.scryfall.io/large/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721'
+        },
+        {
+          name: 'Snapcaster Mage',
+          type: 'Creature',
+          text: 'Flash. When Snapcaster Mage enters the battlefield, target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost.',
+          cost: 2,
+          power: 2,
+          toughness: 1,
+          imageUrl: 'https://cards.scryfall.io/large/front/2/b/2b73d294-6ab1-4051-9b0f-d8e335d37674.jpg?1626097096'
+        },
+        {
+          name: 'Remand',
+          type: 'Instant',
+          text: 'Counter target spell. If that spell is countered this way, put it into its owners hand instead of into that players graveyard.',
+          cost: 1,
+          power: 0,
+          toughness: 0,
+          imageUrl: 'https://miro.medium.com/max/672/0*WtTRy5c4r3h_JFVB.jpg'
+        }
+      ]
     },
     {
       name: 'Vampire Deck',
@@ -181,7 +291,62 @@ export class DecksPageComponent implements OnInit {
       total_value: '60000',
       created_at: new Date("2019-01-16"),
       last_modified: new Date("2019-01-16"),
-      cards: []
+      cards: [
+        {
+          name: 'Goblin Guide',
+          type: 'Creature',
+          text: 'Haste. Whenever Goblin Guide attacks, defending player reveals the top card of their library',
+          cost: 1,
+          power: 2,
+          toughness: 2,
+          imageUrl: 'https://www.bazaargames.nl/images/cards/l/zen/goblin_guide.jpg'
+        },
+        {
+          name: 'Lightning Bolt',
+          type: 'Instant',
+          text: 'Lightning Bolt deals 3 damage to any target.',
+          cost: 1,
+          power: 0,
+          toughness: 0,
+          imageUrl: 'https://cards.scryfall.io/large/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721'
+        },
+        {
+          name: 'Goblin Guide',
+          type: 'Creature',
+          text: 'Haste. Whenever Goblin Guide attacks, defending player reveals the top card of their library',
+          cost: 1,
+          power: 2,
+          toughness: 2,
+          imageUrl: 'https://www.bazaargames.nl/images/cards/l/zen/goblin_guide.jpg'
+        },
+        {
+          name: 'Lightning Bolt',
+          type: 'Instant',
+          text: 'Lightning Bolt deals 3 damage to any target.',
+          cost: 1,
+          power: 0,
+          toughness: 0,
+          imageUrl: 'https://cards.scryfall.io/large/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721'
+        },
+        {
+          name: 'Snapcaster Mage',
+          type: 'Creature',
+          text: 'Flash. When Snapcaster Mage enters the battlefield, target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost.',
+          cost: 2,
+          power: 2,
+          toughness: 1,
+          imageUrl: 'https://cards.scryfall.io/large/front/2/b/2b73d294-6ab1-4051-9b0f-d8e335d37674.jpg?1626097096'
+        },
+        {
+          name: 'Remand',
+          type: 'Instant',
+          text: 'Counter target spell. If that spell is countered this way, put it into its owners hand instead of into that players graveyard.',
+          cost: 1,
+          power: 0,
+          toughness: 0,
+          imageUrl: 'https://miro.medium.com/max/672/0*WtTRy5c4r3h_JFVB.jpg'
+        }
+      ]
     }
   ]
 
