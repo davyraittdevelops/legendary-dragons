60ef65c6e7d6f4dc70ab1e287ebec4d793f0e812
Evripidou <EvripidouNL@gmail.com>
2023-01-12 16:26:33 +0100

    Add NGRX to InventoryPage

--- legendary-dragons-frontend/src/app/components/inventory-page/inventory-page.component.ts
+++ legendary-dragons-frontend/src/app/components/inventory-page/inventory-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, tap } from 'rxjs';
+import { AppState } from 'src/app/app.state';
+import { Inventory } from 'src/app/models/inventory.model';
+import { getInventory } from 'src/app/ngrx/inventory/inventory.actions';
+import { errorSelector, inventorySelector, isLoadingSelector } from 'src/app/ngrx/inventory/inventory.selectors';
 import { Card } from "../../models/card.model";
 
 @Component({
@@ -8,155 +14,23 @@ import { Card } from "../../models/card.model";
 })
 
 export class InventoryPageComponent implements OnInit {
+  inventory$: Observable<Inventory>;
+  isLoading$: Observable<boolean>;
+  hasError$: Observable<boolean>;
 
-  public CARD_DATA: Card[] = [
-    // {
-    //   card_name: 'Goblin Guide',
-    //   set_type: 'Creature',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://www.bazaargames.nl/images/cards/l/zen/goblin_guide.jpg',
-    //   card_faces: [
-    //     {
-    //       oracle_text: "Haste. Whenever Goblin Guide attacks, defending player reveals the top card of their library",
-    //       image_url: "",
-    //     }
-    //   ]
-    // },
-    // {
-    //   card_name: 'Lightning Bolt',
-    //   set_type: 'Instant',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://cards.scryfall.io/large/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721',
-    //   card_faces: [
-    //     {
-    //       oracle_text: "Lightning Bolt deals 3 damage to any target.",
-    //       image_url: "",
-    //     }
-    //   ]
-    // },
-    // {
-    //   card_name: 'Goblin Guide',
-    //   set_type: 'Creature',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://www.bazaargames.nl/images/cards/l/zen/goblin_guide.jpg',
-    //   card_faces: [
-    //     {
-    //       oracle_text: "Haste. Whenever Goblin Guide attacks, defending player reveals the top card of their library",
-    //       image_url: "",
-    //     }
-    //   ]
-    // },
-    // {
-    //   card_name: 'Lightning Bolt',
-    //   set_type: 'Instant',
-    //   text: 'Lightning Bolt deals 3 damage to any target.',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://cards.scryfall.io/large/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721'
-    // },
-    // {
-    //   card_name: 'Snapcaster Mage',
-    //   set_type: 'Creature',
-    //   text: 'Flash. When Snapcaster Mage enters the battlefield, target instant or sorcery card in your graveyard gains flashback until end of turn. The flashback cost is equal to its mana cost.',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://cards.scryfall.io/large/front/2/b/2b73d294-6ab1-4051-9b0f-d8e335d37674.jpg?1626097096'
-    // },
-    // {
-    //   card_name: 'Remand',
-    //   set_type: 'Instant',
-    //   text: 'Counter target spell. If that spell is countered this way, put it into its owners hand instead of into that players graveyard.',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://miro.medium.com/max/672/0*WtTRy5c4r3h_JFVB.jpg'
-    // },
-    // {
-    //   card_name: 'Bloodbraid Elf',
-    //   set_type: 'Creature',
-    //   text: 'Haste. Cascade',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://images.saymedia-content.com/.image/t_share/MTc0NDYwNzc3ODAxODUyMjY0/top-magic-the-gathering-cards-of-all-time.jpg'
-    // },
-    // {
-    //   card_name: 'Tarmogoyf',
-    //   set_type: 'Creature',
-    //   text: 'Tarmogoyfs power is equal to the number of card types among cards in all graveyards and its toughness is equal to that number plus 1.',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://cdn1.dotesports.com/wp-content/uploads/2021/07/27155700/Irencrag-Pyromancer.jpg'
-    // },
-    // {
-    //   card_name: 'Liliana of the Veil',
-    //   set_type: 'Planeswalker',
-    //   text: '+1: Each player discards a card. -2: Target player sacrifices a creature. -6: Separate all permanents target player controls into two piles. That player sacrifices all permanents in the pile of their choice.',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://media.wizards.com/2017/xln/en_yIHG7SYDro.png'
-    // },
-    // {
-    //   card_name: 'Bloodbraid Elf',
-    //   set_type: 'Creature',
-    //   text: 'Haste. Cascade',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://images.saymedia-content.com/.image/t_share/MTc0NDYwNzc3ODAxODUyMjY0/top-magic-the-gathering-cards-of-all-time.jpg'
-    // },
-    // {
-    //   card_name: 'Bloodbraid Elf',
-    //   set_type: 'Creature',
-    //   text: 'Haste. Cascade',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://images.saymedia-content.com/.image/t_share/MTc0NDYwNzc3ODAxODUyMjY0/top-magic-the-gathering-cards-of-all-time.jpg'
-    // },
-    // {
-    //   card_name: 'Bloodbraid Elf',
-    //   set_type: 'Creature',
-    //   text: 'Haste. Cascade',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://images.saymedia-content.com/.image/t_share/MTc0NDYwNzc3ODAxODUyMjY0/top-magic-the-gathering-cards-of-all-time.jpg'
-    // },
-    // {
-    //   card_name: 'Bloodbraid Elf',
-    //   set_type: 'Creature',
-    //   text: 'Haste. Cascade',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://images.saymedia-content.com/.image/t_share/MTc0NDYwNzc3ODAxODUyMjY0/top-magic-the-gathering-cards-of-all-time.jpg'
-    // },
-    // {
-    //   card_name: 'Bloodbraid Elf',
-    //   set_type: 'Creature',
-    //   text: 'Haste. Cascade',
-    //   rarity: "rare",
-    //   released_at: "2022-04-29",
-    //   prices: "1",
-    //   image_url: 'https://images.saymedia-content.com/.image/t_share/MTc0NDYwNzc3ODAxODUyMjY0/top-magic-the-gathering-cards-of-all-time.jpg'
-    // }
-  ];
+  public CARD_DATA: Card[] = [];
 
-  constructor() {
+  constructor(private appStore: Store<AppState>) {
+    this.isLoading$ = this.appStore.select(isLoadingSelector);
+    this.hasError$ = this.appStore.select(errorSelector);
+    this.inventory$ = this.appStore.select(inventorySelector);
+    this.inventory$.pipe(
+      tap(data => console.log(data))
+    )
   }
 
   ngOnInit(): void {
+    // Todo assign the right inventoryId
+    this.appStore.dispatch(getInventory({inventoryId: ""}))
   }
-
 }
--- legendary-dragons-frontend/src/app/ngrx/inventory/inventory.selectors.ts
+++ legendary-dragons-frontend/src/app/ngrx/inventory/inventory.selectors.ts
@@ -4,5 +4,5 @@ import { AppState } from "src/app/app.state";
 export const selectInventory = (state: AppState) => state.inventory
 
 export const isLoadingSelector = createSelector(selectInventory, (state) => state.isLoading);
-export const inventoryErrorSelector = createSelector(selectInventory, (state) => state.hasError);
+export const errorSelector = createSelector(selectInventory, (state) => state.hasError);
 export const inventorySelector = createSelector(selectInventory, (state) => state.inventory);
