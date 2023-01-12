f9f5535ab607790f7ffda3e78814fdb91adb85e6
Joost Konst <AJ.Konst@student.han.nl>
2023-01-12 16:30:32 +0100

    Added functional tests for search card by name

--- legendary-dragons-frontend/cypress/e2e/search-cards-by-keyword.cy.ts
+++ legendary-dragons-frontend/cypress/e2e/search-cards-by-keyword.cy.ts
@@ -19,7 +19,7 @@ describe("Search 'Conclave Mentor' card", () => {
     keywordSearchInput.type("Conclave mentor");
     keywordSearchInput.type("{enter}");
     //assert
-    cy.get("table > tbody", {timeout: 5000}).should("be.visible");
+    cy.get("table > tbody").should("be.visible");
     cy.get("table > tbody").within(() => {
       cy.get("tr").should("have.length", 2);
       cy.get("tr").eq(0).find("td").eq(0).contains("Conclave Mentor");
