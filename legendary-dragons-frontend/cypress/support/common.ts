ad93cee91c93474333fbd627f819de7a32a69fa5
Joost Konst <AJ.Konst@student.han.nl>
2023-01-12 16:39:59 +0100

    Add visit dashboard to login function

--- legendary-dragons-frontend/cypress/support/common.ts
+++ legendary-dragons-frontend/cypress/support/common.ts
@@ -3,4 +3,5 @@ export const loginUser = () => {
   cy.get("input[name=email]").type("oopamxicbrujagrfvi@tmmcv.com")
   cy.get("input[name=password]").type("verySecurePassw0rd!").blur()
   cy.get("form").submit()
+  cy.visit("/dashboard");
 }
