d1067246ad703477b3045fbb9c4d48ce2fa78424
Joost Konst <AJ.Konst@student.han.nl>
2023-01-12 17:04:20 +0100

    Add login to before each and logout to after each

--- legendary-dragons-frontend/cypress/support/common.ts
+++ legendary-dragons-frontend/cypress/support/common.ts
@@ -1,7 +1,10 @@
 export const loginUser = () => {
   cy.visit('/login');
-  cy.get("input[name=email]").type("oopamxicbrujagrfvi@tmmcv.com")
-  cy.get("input[name=password]").type("verySecurePassw0rd!").blur()
-  cy.get("form").submit()
-  cy.visit("/dashboard");
+  cy.get("input[name=email]").type("oopamxicbrujagrfvi@tmmcv.com");
+  cy.get("input[name=password]").type("verySecurePassw0rd!").blur();
+  cy.get("form").submit();
+}
+
+export const logout = () => {
+  cy.get("button[name=logout]").click();
 }
