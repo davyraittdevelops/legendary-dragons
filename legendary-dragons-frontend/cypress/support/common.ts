5aa7c8d219b58055d1630aeb06ac955f7afdacdc
Joost Konst <AJ.Konst@student.han.nl>
2023-01-12 16:30:32 +0100

    Added functional tests for search card by name

---
+++ legendary-dragons-frontend/cypress/support/common.ts
@@ -0,0 +1,6 @@
+export const loginUser = () => {
+  cy.visit('/login');
+  cy.get("input[name=email]").type("oopamxicbrujagrfvi@tmmcv.com")
+  cy.get("input[name=password]").type("verySecurePassw0rd!").blur()
+  cy.get("form").submit()
+}
