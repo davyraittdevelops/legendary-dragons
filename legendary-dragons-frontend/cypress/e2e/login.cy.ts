f4af8626e11f60ce6c792b1f2af8c49eb6f99a80
Joost Konst <AJ.Konst@student.han.nl>
2023-01-13 15:18:06 +0100

    Add proxyConfig option to serve-coverage command

--- legendary-dragons-frontend/cypress/e2e/login.cy.ts
+++ legendary-dragons-frontend/cypress/e2e/login.cy.ts
@@ -1,39 +1,40 @@
 describe('Login user', () => {
   it('passes', () => {
     //arrange
-    cy.visit("/login")
-    let emailInput = cy.get("input[name=email]")
-    let passwordInput = cy.get("input[name=password]")
-    let loginForm = cy.get("form")
+    cy.visit("/login");
+    let emailInput = cy.get("input[name=email]");
+    let passwordInput = cy.get("input[name=password]");
+    let loginForm = cy.get("form");
 
     //act
-    emailInput.type("oopamxicbrujagrfvi@tmmcv.com")
-    passwordInput.type("verySecurePassw0rd!").blur()
-    loginForm.submit()
+    emailInput.type("oopamxicbrujagrfvi@tmmcv.com");
+    passwordInput.type("verySecurePassw0rd!").blur();
+    loginForm.submit();
 
     //assert
-    cy.get("input[name=email]").should('contain.value', "oopamxicbrujagrfvi@tmmcv.com")
-    cy.get("input[name=password]").should('contain.value', "verySecurePassw0rd!")
-  })
+    cy.get("input[name=email]").should('contain.value', "oopamxicbrujagrfvi@tmmcv.com");
+    cy.get("input[name=password]").should('contain.value', "verySecurePassw0rd!");
+    cy.location('pathname').should('eq', '/dashboard');
+  });
 });
 
 describe('Login user with incorrect password', () => {
   it('passes', () => {
     //arrange
-    cy.visit("/login")
-    let emailInput = cy.get("input[name=email]")
-    let passwordInput = cy.get("input[name=password]")
-    let loginForm = cy.get("form")
+    cy.visit("/login");
+    let emailInput = cy.get("input[name=email]");
+    let passwordInput = cy.get("input[name=password]");
+    let loginForm = cy.get("form");
 
     //act
-    emailInput.type("oopamxicbrujagrfvi@tmmcv.com")
-    passwordInput.type("incorrectPassword").blur()
-    loginForm.submit()
+    emailInput.type("oopamxicbrujagrfvi@tmmcv.com");
+    passwordInput.type("incorrectPassword").blur();
+    loginForm.submit();
 
     //assert
-    cy.get("input[name=email]").should('contain.value', "oopamxicbrujagrfvi@tmmcv.com")
-    cy.get("input[name=password]").should('contain.value', "incorrectPassword")
+    cy.get("input[name=email]").should('contain.value', "oopamxicbrujagrfvi@tmmcv.com");
+    cy.get("input[name=password]").should('contain.value', "incorrectPassword");
 
-    cy.get("form").should("contain.text", "Incorrect email/password provided.")
-  })
-})
+    cy.get("form").should("contain.text", "Incorrect email/password provided.");
+  });
+});
