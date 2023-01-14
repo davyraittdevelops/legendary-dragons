2b6415a54b9bb051588e0bf52c8f22296aa336ca
Davy Raitt <davyraitt2@hotmail.com>
2023-01-14 11:33:54 +0100

    delete card lambda works. input:  inventory_id , inventory_card_id

--- inventory/functions/remove_card_from_inventory/app.py
+++ inventory/functions/remove_card_from_inventory/app.py
@@ -20,30 +20,26 @@ def lambda_handler(event, context):
     body = json.loads(event["body"])
     logger.info(f"Received body {body}")
 
+    inventory_card_id = body['inventory_card_id']
+    inventory_id = body['inventory_id']
+
     logger.info(f"Removing inventory card from DynamoDB table")
-    table = dynamodb.Table("inventories")
 
     try:
-        table.delete_item(
+        table = dynamodb.Table("inventories")
+        result = table.delete_item(
         Key={
-            "PK": 'INVENTORY_CARD#' + 'aa0054af-96a0-4246-a4fb-92b92c171492',
-            "SK": 'INVENTORY#' + 'd305e5e8-763d-4c6f-83be-7e5eb39bd2f9'
+            "PK": 'INVENTORY_CARD#' + inventory_card_id,
+            "SK": 'INVENTORY#' + inventory_id
         },
         ReturnValues='ALL_OLD'
     )
-        print('Card with ID ' + 'aa0054af-96a0-4246-a4fb-92b92c171492' + ' has been deleted.')
+        logger.info(f"Deleted card from table, result is  {result}")
+        
     except Exception as error:
         print(error)
         print('Error deleting card with ID ' + 'd305e5e8-763d-4c6f-83be-7e5eb39bd2f9')
-
-    # user_id = event["requestContext"]["authorizer"]["userId"]
-    # inventory_id = body["inventory_id"]
-    # card_id = str(uuid.uuid4())
-    # card = body["inventory_card"]
-    # now = datetime.utcnow().isoformat()
-    # pk = f"INVENTORY_CARD#{card_id}"
-    # sk = f"INVENTORY#{inventory_id}"
-
-
    
-    return {"statusCode": 200}
+    return {
+        "statusCode": 200,
+        }
