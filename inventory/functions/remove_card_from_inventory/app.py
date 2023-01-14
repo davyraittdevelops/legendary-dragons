ad5b45a75b5cebe79b17af42e19d24a1f332c8fb
Davy Raitt <davyraitt2@hotmail.com>
2023-01-14 11:19:48 +0100

    lambda works with dummy data

--- inventory/functions/remove_card_from_inventory/app.py
+++ inventory/functions/remove_card_from_inventory/app.py
@@ -18,8 +18,23 @@ dynamodb = boto3.resource("dynamodb")
 def lambda_handler(event, context):
     """Extract inventory ID and card ID"""
     body = json.loads(event["body"])
-
     logger.info(f"Received body {body}")
+
+    logger.info(f"Removing inventory card from DynamoDB table")
+    table = dynamodb.Table("inventories")
+
+    try:
+        table.delete_item(
+        Key={
+            "PK": 'INVENTORY_CARD#' + 'aa0054af-96a0-4246-a4fb-92b92c171492',
+            "SK": 'INVENTORY#' + 'd305e5e8-763d-4c6f-83be-7e5eb39bd2f9'
+        },
+        ReturnValues='ALL_OLD'
+    )
+        print('Card with ID ' + 'aa0054af-96a0-4246-a4fb-92b92c171492' + ' has been deleted.')
+    except Exception as error:
+        print(error)
+        print('Error deleting card with ID ' + 'd305e5e8-763d-4c6f-83be-7e5eb39bd2f9')
 
     # user_id = event["requestContext"]["authorizer"]["userId"]
     # inventory_id = body["inventory_id"]
@@ -29,7 +44,6 @@ def lambda_handler(event, context):
     # pk = f"INVENTORY_CARD#{card_id}"
     # sk = f"INVENTORY#{inventory_id}"
 
-    logger.info(f"Removing inventory card from DynamoDB table")
 
    
     return {"statusCode": 200}
