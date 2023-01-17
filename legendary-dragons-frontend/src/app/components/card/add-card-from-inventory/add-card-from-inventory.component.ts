cdb4b7c5e69d4cca33fc791d8d4347b70b02377d
Davy Raitt <davyraitt2@hotmail.com>
2023-01-17 21:40:50 +0100

    added modal that loads inventory cards instead of default

---
+++ legendary-dragons-frontend/src/app/components/card/add-card-from-inventory/add-card-from-inventory.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {clearSearchResult} from "../../../ngrx/card/card.actions";
+import {Observable} from "rxjs";
+import {Inventory} from "../../../models/inventory.model";
+import {Store} from "@ngrx/store";
+import {AppState} from "../../../app.state";
+import {errorSelector, inventorySelector, isLoadingSelector} from "../../../ngrx/inventory/inventory.selectors";
+import {getInventory} from "../../../ngrx/inventory/inventory.actions";
+import {MatTableDataSource} from "@angular/material/table";
+import {Card} from "../../../models/card.model";
+
+@Component({
+  selector: 'app-add-card-from-inventory',
+  templateUrl: './add-card-from-inventory.component.html',
+  styleUrls: ['./add-card-from-inventory.component.scss']
+})
+export class AddCardFromInventoryComponent {
+  inventory$: Observable<Inventory>;
+  isLoading$: Observable<boolean>;
+  hasError$: Observable<boolean>;
+  displayedColumns: string[] = ['Image', 'Name', 'AddButton'];
+  dataSource:any;
+
+
+  constructor(private appStore: Store<AppState>, public modalService: NgbModal) {
+    this.isLoading$ = this.appStore.select(isLoadingSelector);
+    this.hasError$ = this.appStore.select(errorSelector);
+    this.inventory$ = this.appStore.select(inventorySelector);
+  }
+
+
+  ngOnInit(): void {
+    this.appStore.dispatch(getInventory())
+  }
+
+  open({content}: { content: any }): void {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'xl'}).result.then(
+      () => {},
+      () => {
+      }
+    );
+  }
+
+  applyFilter(event: Event) {
+    console.log('event', event)
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  addCardToDeck(card: Card) {
+    console.log('selected card, card', card)
+
+  }
+
+  addCardToSideDeck(card: Card) {
+    console.log('selected card, card', card)
+  }
+}
