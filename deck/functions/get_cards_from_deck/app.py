984a697f2fe6c3ff562e86fac2fd18d0a2584563
Davy Raitt <davyraitt2@hotmail.com>
2023-01-17 19:51:05 +0100

    Done with lambda: get_cards_from_deck

--- deck/functions/get_cards_from_deck/app.py
+++ deck/functions/get_cards_from_deck/app.py
@@ -22,29 +22,31 @@ def lambda_handler(event, context):
     connection_id = event["requestContext"]["connectionId"]
     domain_name = event["requestContext"]["domainName"]
     stage = event["requestContext"]["stage"]
-
     endpoint = f"https://{domain_name}/{stage}"
-    output = {
-        "event_type": "GET_DECK_RESULT",
-        "data": []
-    }
-    logger.info(f"Request will be made to {endpoint}")
-
     apigateway = boto3.client("apigatewaymanagementapi", endpoint_url=endpoint)
 
-    user_id = event["requestContext"]["authorizer"]["userId"]
+    body = json.loads(event["body"])
+    deck_id = body['deck_id']
+    
+    logger.info("Querying the table to find the deck cards")
+    logger.info(f"Querying with deck_id {deck_id}")
 
-    logger.info("Searching for decks in user %s", user_id)
+    try:
+        decks = table.query(
+            KeyConditionExpression=Key("GSI1_PK").eq(f"DECK#{deck_id}") 
+            &
+            Key("GSI1_SK").begins_with("DECK_CARD#"),
+            IndexName="GSI1"
+        )
+        logger.info(f"Querying for deck cards succesful, with deck_id {deck_id}")
+        print(decks)
+    except Exception as e:
+        logger.info(f"Exception retrieving cards! {e}")
+    
 
-    decks = table.query(
-        KeyConditionExpression=Key("GSI1_PK").eq(f"USER#{user_id}") &
-        Key("GSI1_SK").begins_with("DECK#"),
-        IndexName="GSI1"
-    )
-    output["data"] = decks["Items"]
     apigateway.post_to_connection(
         ConnectionId=connection_id,
-        Data=json.dumps(output, cls=DecimalEncoder)
+        Data=json.dumps(decks, cls=DecimalEncoder)
     )
     return {"statusCode": 200}
 
