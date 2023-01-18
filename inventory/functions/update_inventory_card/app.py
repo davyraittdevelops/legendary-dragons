892e33fdd782e31784421e843f9a7168e8fad1ec
joost1808 <46594731+joost1808@users.noreply.github.com>
2023-01-18 10:13:44 +0100

    Add 'AddCardToDeck' and 'UpdateInventoryCard' lambda functions (#39)
    * Add 'AddCardToDeck' and 'UpdateInventoryCard0 lambda' functions
    
    * changes made
    
    * Changed conditional expression + added integration test
    
    Co-authored-by: Joost Konst <AJ.Konst@student.han.nl>
    Co-authored-by: Davy Raitt <davyraitt2@hotmail.com>

---
+++ inventory/functions/update_inventory_card/app.py
@@ -0,0 +1,33 @@
+import json
+import logging
+import os
+
+import boto3
+from aws_xray_sdk.core import patch_all
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+if "DISABLE_XRAY" not in os.environ:
+  patch_all()
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.getenv("TABLE_NAME"))
+
+def lambda_handler(event, context):
+  """Updates Inventory Card entry"""
+  inventory_card = event["detail"]["inventory_card"]
+
+  try:
+    table.put_item(Item=inventory_card,
+      ConditionExpression='attribute_exists(PK) AND attribute_exists(SK)'
+    )
+  except dynamodb.meta.client.exceptions.ConditionalCheckFailedException as e:
+    error = str(e)[str(e).index(":") + 1:len(str(e))]
+    logger.info(f"Registration failed: {error}")
+    return {
+      "statusCode": 400,
+      "body": json.dumps({"message": error.strip()})
+    }
+
+  return {"statusCode": 200}
---
+++ inventory/tests/integration/test_update_inventory_card.py
@@ -0,0 +1,133 @@
+import json
+import os
+from unittest.mock import patch
+
+import boto3
+import pytest
+from boto3.dynamodb.conditions import Key
+from moto import mock_dynamodb
+
+CONNECTION_ID = "abcdefg"
+TABLE_NAME = "test_inventories"
+OS_ENV = {
+  "AWS_ACCESS_KEY_ID": "testing",
+  "AWS_SECRET_ACCESS_KEY": "testing",
+  "AWS_SECURITY_TOKEN": "testing",
+  "AWS_SESSION_TOKEN": "testing",
+  "AWS_DEFAULT_REGION": "us-east-1",
+  "DISABLE_XRAY": "True",
+  "TABLE_NAME": TABLE_NAME
+}
+
+@pytest.fixture()
+def table_definition():
+  return {
+    "TableName": TABLE_NAME,
+    "KeySchema": [
+      {"AttributeName": "PK", "KeyType": "HASH"},
+      {"AttributeName": "SK", "KeyType": "RANGE"}
+    ],
+    "AttributeDefinitions": [
+      {"AttributeName": "PK", "AttributeType": "S"},
+      {"AttributeName": "SK", "AttributeType": "S"},
+      {"AttributeName": "GSI1_PK", "AttributeType": "S"},
+      {"AttributeName": "GSI1_SK", "AttributeType": "S"},
+    ],
+    "GlobalSecondaryIndexes": [
+      {
+        "IndexName": "GSI1",
+        "Projection": {"ProjectionType": "ALL"},
+        "KeySchema": [
+          {"AttributeName": "GSI1_PK", "KeyType": "HASH"},
+          {"AttributeName": "GSI1_SK", "KeyType": "RANGE"}
+        ]
+      }
+    ],
+    "BillingMode": "PAY_PER_REQUEST"
+  }
+
+@pytest.fixture()
+def bus_event():
+  return {
+    "source": "legdragons.decks.add_card_to_deck",
+    "detail-type": "CARD_ADDED_TO_DECK",
+    "detail": {
+      "inventory_card": {
+        "PK": "INVENTORY_CARD#1",
+        "SK": "INVENTORY#1",
+        "entity_type": "INVENTORY_CARD",
+        "card_name": "Swords of Doom",
+        "card_id": "1",
+        "user_id": "1",
+        "inventory_id": "123",
+        "oracle_id": "oracle-123",
+        "colors": ["R"],
+        "prices": {"usd": "0.05"},
+        "rarity": "meta",
+        "quality": "rare",
+        "scryfall_id": "scryfall-1",
+        "image_url": "example-image-url.com",
+        "deck_location": "deck-1",
+        "GSI1_PK": "INVENTORY#1",
+        "GSI1_SK": "INVENTORY_CARD#1",
+      }
+    }
+  }
+
+@patch.dict(os.environ, OS_ENV, clear=True)
+@mock_dynamodb
+def test_lamda_handler_update_card_deck_location(bus_event, table_definition):
+  # Arrange
+  # 1. Create the DynamoDB Table
+  dynamodb = boto3.resource("dynamodb")
+  table = dynamodb.create_table(**table_definition)
+  table.put_item(Item={
+    "PK": "INVENTORY_CARD#1",
+    "SK": "INVENTORY#1",
+    "entity_type": "INVENTORY_CARD",
+    "card_name": "Swords of Doom",
+    "card_id": "1",
+    "user_id": "1",
+    "inventory_id": "123",
+    "oracle_id": "oracle-123",
+    "colors": ["R"],
+    "prices": {"usd": "0.05"},
+    "rarity": "meta",
+    "quality": "rare",
+    "scryfall_id": "scryfall-1",
+    "image_url": "example-image-url.com",
+    "deck_location": "unassigned",
+    "GSI1_PK": "INVENTORY#1",
+    "GSI1_SK": "INVENTORY_CARD#1",
+  })
+  # Act
+  from functions.update_inventory_card import app
+  response = app.lambda_handler(bus_event, {})
+
+  inventory_cards = table.query(
+      KeyConditionExpression=Key("PK").eq("INVENTORY_CARD#1") &
+                             Key("SK").begins_with("INVENTORY#1")
+  )["Items"]
+  # Assert
+  assert response["statusCode"] == 200
+  assert len(inventory_cards) == 1
+  assert inventory_cards[0]["card_id"] == "1"
+  assert inventory_cards[0]["entity_type"] == "INVENTORY_CARD"
+  assert inventory_cards[0]["deck_location"] == "deck-1"
+
+
+@patch.dict(os.environ, OS_ENV, clear=True)
+@mock_dynamodb
+def test_lamda_handler_update_card_deck_location(bus_event, table_definition):
+  # Arrange
+  # 1. Create the DynamoDB Table
+  dynamodb = boto3.resource("dynamodb")
+  dynamodb.create_table(**table_definition)
+
+  # Act
+  from functions.update_inventory_card import app
+  response = app.lambda_handler(bus_event, {})
+
+  # Assert
+  assert response["statusCode"] == 400
+  assert json.loads(response["body"])["message"] == "The conditional request failed"
