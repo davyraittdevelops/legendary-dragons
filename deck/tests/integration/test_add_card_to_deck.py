e48552acdb60552f785c468f924d3a28494ed99c
Salt <59624679+SaltMining@users.noreply.github.com>
2023-01-18 15:06:18 +0100

    Feature/add sidedeck card (#43)
    * changed function so it checks on deck_type
    
    * changed test for add card to deck
    
    * added assert for SK to check whether it has been added to side deck

--- deck/tests/integration/test_add_card_to_deck.py
+++ deck/tests/integration/test_add_card_to_deck.py
@@ -60,7 +60,7 @@ def websocket_event():
     },
     "body": json.dumps({
       "action": "addCardToDeckReq",
-      "deck_name": "deck-123",
+      "deck_type": "deck",
       "deck_id": "123",
       "inventory_card": {
         "card_name": "Swords of Doom",
@@ -125,3 +125,57 @@ def test_lamda_handler_success(websocket_event, table_definition):
   assert deck_card
   assert deck_card["deck_id"] == "123"
   assert deck_card["entity_type"] == "DECK_CARD"
+
+
+@patch.dict(os.environ, OS_ENV, clear=True)
+@mock_dynamodb
+@mock_events
+@mock_sqs
+def test_lamda_handler_sidedeck(websocket_event, table_definition):
+  # Arrange
+  # 1. Create the DynamoDB Table
+  dynamodb = boto3.resource("dynamodb")
+  table = dynamodb.create_table(**table_definition)
+
+  # 2. Create SQS and EventBridge
+  sqs = boto3.resource("sqs")
+  queue = sqs.create_queue(QueueName="test-output-queue")
+  client = boto3.client("events")
+  client.create_event_bus(Name=OS_ENV["EVENT_BUS_NAME"])
+  client.put_rule(
+      Name="test-rule", EventBusName=OS_ENV["EVENT_BUS_NAME"],
+      EventPattern=json.dumps(
+          {"detail-type": ["CARD_ADDED_TO_DECK"], "source": ["legdragons.deck.add_card_to_deck"]}
+      )
+  )
+  client.put_targets(
+      Rule="test-rule", EventBusName=OS_ENV["EVENT_BUS_NAME"],
+      Targets=[{
+        "Id": "testing-target",
+        "Arn": queue.attributes.get("QueueArn")
+      }]
+  )
+  body = json.loads(websocket_event["body"])
+  body["deck_type"] = "side_deck"
+  websocket_event["body"] = json.dumps(body)
+
+  # Act
+  from functions.add_card_to_deck import app
+  response = app.lambda_handler(websocket_event, {})
+  messages = queue.receive_messages(MaxNumberOfMessages=2)
+  stream_message = json.loads(messages[0].body)
+
+  deck_card = table.query(
+      KeyConditionExpression=Key("GSI1_PK").eq("DECK#123#SIDE_DECK") &
+                             Key("GSI1_SK").begins_with("DECK_CARD"),
+      IndexName="GSI1"
+  )["Items"][0]
+
+  # Assert
+  assert response["statusCode"] == 200
+  assert len(messages) == 1
+  assert stream_message["detail-type"] == "CARD_ADDED_TO_DECK"
+  assert deck_card
+  assert deck_card["deck_id"] == "123"
+  assert deck_card["entity_type"] == "DECK_CARD"
+  assert deck_card["SK"] == "DECK#123#SIDE_DECK"
