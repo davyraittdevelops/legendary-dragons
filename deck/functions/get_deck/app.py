0a876435434058e32225528c9209f71c581a3c49
Davy Raitt <60343123+Davyraitt@users.noreply.github.com>
2023-01-19 16:17:18 +0100

    Feature refactor get cards from deck (#49)
    * added base for get card function, and fix for get_cards_from_deck
    
    * fix for get_cards_from_deck
    
    * get card from cards table done, also fixed update from scryfall func
    
    * checked code and done
    
    * modified get_cards_from_deck to get_deck
    
    * modified to also include side_deck_cards
    
    * edited import in tests
    
    * using extend in stead of nested for loop
    
    * made requested changes from PR
    
    * removed unused var and replaced back to query
    
    Co-authored-by: Davy Raitt <davyraitt2@hotmail.com>

--- deck/functions/get_deck/app.py
+++ deck/functions/get_deck/app.py
@@ -3,13 +3,12 @@ import logging
 import os
 import boto3
 from decimal import Decimal
+from datetime import datetime
 from boto3.dynamodb.conditions import Key
 from aws_xray_sdk.core import patch_all
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-INDEX_NAME = "GSI1"
 
 if "DISABLE_XRAY" not in os.environ:
     patch_all()
@@ -17,7 +16,6 @@ if "DISABLE_XRAY" not in os.environ:
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(os.getenv("TABLE_NAME"))
-
 
 def lambda_handler(event, context):
     connection_id = event["requestContext"]["connectionId"]
@@ -28,41 +26,44 @@ def lambda_handler(event, context):
 
     body = json.loads(event["body"])
     deck_id = body['deck_id']
-
+    
     logger.info("Querying the table to find the deck cards")
     logger.info(f"Querying with deck_id {deck_id}")
 
     try:
-        pk = f"{INDEX_NAME}_PK"
-        sk = f"{INDEX_NAME}_SK"
-        pk_value = f"DECK#{deck_id}"
-        sk_value = "DECK_CARD#"
+        deck = table.query(
+            KeyConditionExpression=Key("PK").eq(f"DECK#{deck_id}") 
+            &
+            Key("SK").begins_with("USER#")
+        )['Items']
+        logger.info(f"Querying for deck succesful, with deck_id {deck_id}")
 
         deck_cards = table.query(
-            KeyConditionExpression=Key(pk).eq(pk_value)
+            KeyConditionExpression=Key("GSI1_PK").eq(f"DECK#{deck_id}") 
             &
-            Key(sk).begins_with(sk_value),
-            IndexName=INDEX_NAME
+            Key("GSI1_SK").begins_with("DECK_CARD#"),
+            IndexName="GSI1"
         )['Items']
+        logger.info(f"Querying for deck cards succesful, with deck_id {deck_id}")
 
         side_deck_cards = table.query(
-            KeyConditionExpression=Key(pk).eq(f"{pk_value}#SIDE_DECK")
-            &
-            Key(sk).begins_with(sk_value),
-            IndexName=INDEX_NAME
+        KeyConditionExpression=Key("GSI1_PK").eq(f"DECK#{deck_id}#SIDE_DECK")
+        &
+        Key("GSI1_SK").begins_with("DECK_CARD#"),
+        IndexName="GSI1"
         )['Items']
-
-        logger.info(f"Querying for deck cards successful, with deck_id {deck_id}")
     except Exception as e:
         logger.info(f"Exception retrieving cards! {e}")
-
+    
+    
     output = {
-        "event_type": "GET_DECK_CARDS_RESULT",
-        "deck_id": deck_id,
-        "data": {
+        "event_type": "GET_DECK_RESULT",
+        "data" : {
+            "deck": deck,
             "deck_cards": deck_cards,
             "side_deck_cards": side_deck_cards
         }
+
     }
 
     apigateway.post_to_connection(
@@ -70,7 +71,6 @@ def lambda_handler(event, context):
         Data=json.dumps(output, cls=DecimalEncoder)
     )
     return {"statusCode": 200}
-
 
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
