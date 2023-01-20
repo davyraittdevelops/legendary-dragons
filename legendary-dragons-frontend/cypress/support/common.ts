bb17dfe1717771192b4dd012912bacc733cf4cde
joost1808 <46594731+joost1808@users.noreply.github.com>
2023-01-20 11:30:24 +0100

    Add 'add card to deck' and 'add card to side-deck' functional tests (#59)
    * Create ngrx for get decks
    
    * Make use of selects + some styling changes
    
    * Dix reducer on remove of a deck
    
    * added modal that loads inventory cards instead of default
    
    * stash changes ngrx
    
    * stash changes
    
    * Add NGRX for add card to deck and remove card from deck
    
    * Add deck-type for sidedeck
    
    * function get cards of deck now returns deck_id
    
    * Rearrange code layout
    
    * Refactor all components into sub directories
    
    * Get Cards from Deck fixxed
    
    * Fix for get card from decks
    
    * Add removeCardFromDeck in NGRX and fix for getCardsFromDeck
    
    * Replace 'mergeMap' with 'switchMap' and fix getCardsFromSideDeck
    
    * Add before each methods for functionel tests frontend
    
    * Add 'AddCardToDeck' functional test frontend
    
    * added modal that loads inventory cards instead of default
    
    * stash changes ngrx
    
    * stash changes
    
    * Add NGRX for add card to deck and remove card from deck
    
    * Add deck-type for sidedeck
    
    * Refactor all components into sub directories
    
    * Add removeCardFromDeck in NGRX and fix for getCardsFromDeck
    
    * Replace 'mergeMap' with 'switchMap' and fix getCardsFromSideDeck
    
    * Add before each methods for functionel tests frontend
    
    * Add 'AddCardToDeck' functional test frontend
    
    * Change 'Add card to deck' functional test assertions
    
    * Add 'add card to side-deck' functional test frontend
    
    Co-authored-by: Koziad <kostas1999NL@gmail.com>
    Co-authored-by: Davy Raitt <davyraitt2@hotmail.com>
    Co-authored-by: Evripidou <EvripidouNL@gmail.com>
    Co-authored-by: Joost Konst <AJ.Konst@student.han.nl>

--- legendary-dragons-frontend/cypress/support/common.ts
+++ legendary-dragons-frontend/cypress/support/common.ts
@@ -1,9 +1,10 @@
 export const loginUser = () => {
-  cy.visit('/login');
+  cy.visit("/login");
   cy.get("input[name=email]").type("ajdjbxombnywmxvkxu@tmmcv.com");
   cy.get("input[name=password]").type("verySecurePassw0rd!").blur();
   cy.get("form").submit();
-  cy.url().should('contain', '/dashboard');
+  cy.url().should("contain", "/dashboard");
+  cy.reload();
 }
 
 export const logout = () => {
@@ -15,7 +16,43 @@ export const removeAllInventoryCards = () => {
   cy.get("body").then($body => {
     if ($body.find("div[aria-label=collection-card]").length > 0) {
       cy.get("button[name=removeCardFromInventory]").click({multiple: true});
+      cy.wait(1000);
     }
   });
-  cy.get("div[aria-label=collection-card]").should('not.exist');
+  cy.get("div[aria-label=collection-card]").should("not.exist");
+}
+
+export const addCardToInventory = () => {
+  cy.visit("/dashboard");
+  cy.get("button[name=openAddCardModal]").click();
+  cy.get("div[role=document]").should("be.visible");
+  cy.get("input[name=keywordSearch]").type("Swords to plowshares");
+  cy.get("input[name=keywordSearch]").type("{enter}");
+  cy.get("#mat-select-0").click();
+  cy.get("#mat-option-0").click();
+  cy.get("button[name=addCardToInventory]").eq(0).click();
+  cy.get("button[aria-label=Close]").click();
+}
+
+export const removeAllDecks = () => {
+  cy.visit("/dashboard");
+  cy.get("#mat-tab-label-0-1").click();
+  cy.wait(3000);
+  cy.get("body").then($body => {
+    if ($body.find("div[aria-label=deck]").length > 0) {
+      cy.get("button[name=removeDeck]").click({multiple: true});
+      cy.wait(1000);
+    }
+  });
+  cy.get("div[aria-label=deck]").should("not.exist");
+}
+
+export const createDeck = () => {
+  cy.visit("/dashboard");
+  cy.get("#mat-tab-label-0-1").click();
+  cy.get("button[name=openCreateDeckModal]").click();
+  cy.get("div[role=document]").should("be.visible");
+  cy.get("input[name=name]").type("Main");
+  cy.get("input[name=decktype]").type("EDH/Commander");
+  cy.get("button[name=createDeck]").click();
 }
