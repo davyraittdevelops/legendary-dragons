928e6c1bb13369b5866ee2d07795fdccf3e59eba
Joost Konst <AJ.Konst@student.han.nl>
2023-01-20 11:18:58 +0100

    Add 'add card to side-deck' functional test frontend

--- legendary-dragons-frontend/cypress/e2e/add-card-to-deck.cy.ts
+++ legendary-dragons-frontend/cypress/e2e/add-card-to-deck.cy.ts
@@ -28,9 +28,32 @@ describe("Add 'Swords to Plowshares' to deck 'Main'", () => {
     cy.get("button[aria-label=Close]").click();
     cy.wait(3000);
     //assert
-    // cy.get("1 cards").should("be.visible");
     cy.get("div[aria-label=deck-viewport]").should("be.visible");
     cy.get("div[aria-label=deck-viewport]").within(() => {
+      cy.get("div[aria-label=deck-card]").should("exist");
+      cy.get("button[name=openDeckCardDetailsModal]").click();
+    });
+    let deckCardDetails = cy.get("div[role=document]")
+    deckCardDetails.should("be.visible");
+    cy.contains("Swords to Plowshares");
+
+    cy.get("button[aria-label=Close]").click();
+  });
+});
+
+describe("Add 'Swords to Plowshares' to side-deck of deck 'Main'", () => {
+  it('passes', () => {
+    //arrange
+    cy.get("button[name=navigateToDeck]").click();
+    //act
+    cy.get("button[name=openAddDeckCardModal]").click();
+    cy.get("div[role=document]").should("be.visible");
+    cy.get("button[name=addCardToSideDeck]").click();
+    cy.get("button[aria-label=Close]").click();
+    cy.wait(3000);
+    //assert
+    cy.get("div[aria-label=side-deck-viewport]").should("be.visible");
+    cy.get("div[aria-label=side-deck-viewport]").within(() => {
       cy.get("div[aria-label=deck-card]").should("exist");
       cy.get("button[name=openDeckCardDetailsModal]").click();
     });
