df32f8feeea412af4aaee55b143ce71058497d8b
Joost Konst <AJ.Konst@student.han.nl>
2023-01-24 18:51:45 +0100

    Add functionality for updated inventory total value on add and remove

--- inventory/functions/remove_card_from_inventory/app.py
+++ inventory/functions/remove_card_from_inventory/app.py
@@ -1,6 +1,8 @@
 import json
 import logging
 import os
+from decimal import Decimal
+
 import boto3
 from aws_xray_sdk.core import patch_all
 
@@ -33,7 +35,39 @@ def lambda_handler(event, context):
         )
 
         logger.info(f"Deleted card from table, result is {result}")
-    except Exception as error:
-        logger.info('Error deleting card from database: %s', error)
+
+        update_inventory_total_value(user_id, inventory_id, result["Attributes"]["prices"])
+    except Exception as e:
+        logger.info(f"Error deleting card from database: {e}")
 
     return {"statusCode": 200}
+
+def update_inventory_total_value(user_id, inventory_id, card_prices):
+    pk = "USER#" + user_id
+    sk = "INVENTORY#" + inventory_id
+    inventory = table.get_item(
+        Key={
+            "PK": pk,
+            "SK": sk
+        }
+    )["Item"]
+
+    inventory_total_values = dict(sorted(inventory["total_value"].items()))
+    card_prices = dict(sorted(card_prices.items()))
+
+    new_total_values = {}
+    for (key, value), (key2, value2) in zip(inventory_total_values.items(), card_prices.items()):
+        new_value = value - Decimal(value2.replace(',','.')) if value2 else value
+        new_total_values[key] = new_value if new_value >= 0 else 0
+
+    table.update_item(
+        Key={
+            "PK": pk,
+            "SK": sk
+        },
+        ConditionExpression='attribute_exists(PK) AND attribute_exists(SK)',
+        UpdateExpression='set total_value = :new_total_values',
+        ExpressionAttributeValues={
+            ":new_total_values": new_total_values
+        }
+    )
