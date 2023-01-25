f496e3650caceae6f296f0ac5a1300053329d439
Salt <evatang2002@hotmail.com>
2023-01-25 02:50:16 +0100

    setup for both frontend test

---
+++ legendary-dragons-frontend/cypress/e2e/add-and-remove-price.cy.ts
@@ -0,0 +1,29 @@
+import {loginUser, logout, removeAllWishlistCards} from "../support/common";
+
+beforeEach(() => {
+  loginUser();
+  removeAllWishlistCards();
+});
+
+afterEach(() => {
+  logout();
+});
+
+describe('Create and remove a card price alert', () => {
+  it('passes', () => {
+    // Arrange
+    
+
+    // Act - Create
+    
+
+    // Assert - Create
+    
+
+    // Act - Delete
+    
+
+    // Assert - Delete
+    
+  })
+})
