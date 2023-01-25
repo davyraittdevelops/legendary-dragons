cc1cad7f9097a6ed8b2d04b925ca6cc4ba3446ee
Koziad <kostas1999NL@gmail.com>
2023-01-25 14:30:56 +0100

    Check pipeline logs keys

--- card/tests/integration/test_process_card_alert.py
+++ card/tests/integration/test_process_card_alert.py
@@ -1,5 +1,4 @@
 import os
-import json
 import pytest
 import boto3
 from moto import mock_events
@@ -16,9 +15,6 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-
-
-
 OS_ENV = {
     "AWS_ACCESS_KEY_ID": "testing",
     "AWS_SECRET_ACCESS_KEY": "testing",
@@ -27,11 +23,11 @@ OS_ENV = {
     "DISABLE_XRAY":  "True",
     "TESTING":  "True",
     "EVENT_BUS_NAME": "test-event-bus",
-    "TABLE_NAME" : "cards",
-    "COGNITO_CLIENT" : ""
+    "TABLE_NAME": "cards",
+    "COGNITO_CLIENT": ""
  }
 
- 
+
 @pytest.fixture()
 def table_definition():
     return {
@@ -59,9 +55,11 @@ def table_definition():
         "BillingMode": "PAY_PER_REQUEST"
     }
 
+
 @pytest.fixture()
 def event():
     return {'Records': [{ 'body': '{"created_at": "2023-01-24T12:58:18.240549", "entity_type": "ALERT#PRICE", "wishlist_item_id": "c6adf95f-7ebb-4e18-959b-42aa71c3961f", "user_id": "0d62b27d-772a-4895-987c-19ae4fd7fb9f", "alert_id": "23d4f436-a417-4a26-b5b8-c698f11a186b", "card_name": "Acid Web Spider", "price_point": "10", "GSI1_SK": "USER0d62b27d-772a-4895-987c-19ae4fd7fb9f", "SK": "WISHLIST_ITEM#c6adf95f-7ebb-4e18-959b-42aa71c3961f#ALERT#PRICE#23d4f436-a417-4a26-b5b8-c698f11a186b", "PK": "USER#0d62b27d-772a-4895-987c-19ae4fd7fb9f", "last_modified": "2023-01-24T12:58:18.240549", "card_market_id": "242799", "GSI1_PK": "WISHLIST_ITEM#c6adf95f-7ebb-4e18-959b-42aa71c3961f#ALERT#PRICE#23d4f436-a417-4a26-b5b8-c698f11a186b"}'}]}
+
 
 def cognito_pool():
     cognito_client = boto3.client("cognito-idp")
@@ -82,6 +80,7 @@ def cognito_pool():
         "user_pool_id": user_pool_id
     }
 
+
 @patch.dict(os.environ, OS_ENV, clear=True)
 @mock_events
 @mock_dynamodb
@@ -98,7 +97,7 @@ def test_lambda_handler(event, table_definition):
     email = "legendary-dragons@example.test.nl"
     password = "Pytest2022!"
     nickname = "Legendary Dragons"
-    
+
     cognito = cognito_pool()
 
     client_id = cognito["app_client"]["UserPoolClient"]["ClientId"]
@@ -119,27 +118,29 @@ def test_lambda_handler(event, table_definition):
     cognito["client"].admin_confirm_sign_up(
         UserPoolId=cognito["user_pool_id"], Username=email
     )
-    replaced_record = event['Records'][0]['body'].replace('0d62b27d-772a-4895-987c-19ae4fd7fb9f' , email)
+    replaced_record = event['Records'][0]['body'].replace('0d62b27d-772a-4895-987c-19ae4fd7fb9f', email)
     event['Records'][0]['body'] = replaced_record
-    
+
     table.put_item(Item={
         "PK": "CARD#968a25a5-9ec1-47fa-bf1f-e65eb75fdb00",
         "SK": "CARD_FACE#23d4f436-a417-4a26-b5b8-c698f11a186b",
-        "card_name" : "Acid Web Spider",
-        "entity_type" : "CARD",
+        "card_name": "Acid Web Spider",
+        "entity_type": "CARD",
         "GSI1_PK": "CARD_FACE#23d4f436-a417-4a26-b5b8-c698f11a186b",
         "GSI1_SK": "CARD#968a25a5-9ec1-47fa-bf1f-e65eb75fdb00",
-        "oracle_id" : "23d4f436-a417-4a26-b5b8-c698f11a186b",
-        "prices" : {"eur": "0.20"}
+        "oracle_id": "23d4f436-a417-4a26-b5b8-c698f11a186b",
+        "prices": {"eur": "0.20"}
     })
 
     # Act
     from functions.process_card_alert import app
     response = app.lambda_handler(event, {})
 
-    # Assert
-    # ses_backend = ses_backends[DEFAULT_ACCOUNT_ID]
-    messages = ses_backend.sent_messages # sent_messages is a List of Message objects
+    logger.info(ses_backends[DEFAULT_ACCOUNT_ID].keys())
+    # sent_messages is a List of Message objects
+    messages = ses_backend.sent_messages
     message = messages.pop()
-    assert message.body == 'Congratulations! The price alert for the card Acid Web Spider has been triggered. The current card price is 0.20 eur and your alert price value was set to 10.0'
 
+    # Assert
+    assert response["statusCode"] == 201
+    assert message.body == 'Congratulations! The price alert for the card Acid Web Spider has been triggered. The current card price is 0.20 eur and your alert price value was set to 10.0'
